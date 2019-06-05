fa25518a38cdef9ca1a11637deca30a4d4b43baf
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-05 18:19:21 +0000

    Automatically backed up by Learn

--- src/Matrix.js
+++ src/Matrix.js
@@ -12,6 +12,12 @@ export default class Matrix extends Component {
     }
   }
 
+ setSelectedColor = (newColor) => {
+    this.setState({
+      selectedColor: newColor
+    })
+  }
+
   genRow = (vals) => (
     vals.map((val, idx) => <Cell key={idx} color={val} />)
   )
