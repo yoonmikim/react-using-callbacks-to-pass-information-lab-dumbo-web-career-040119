fd25a12e7dc5b3d7a0624e6d10e4ff6e73592ac0
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-05 18:18:23 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import chromeBoi from './data.js'
 import Matrix from './Matrix.js'
-import Cell from './Cell'
-import ColorSelector from './ColorSelector'
 
 ReactDOM.render(
     <Matrix values={chromeBoi.slice(0, 10)} />,
